039c4d70dafb000fabfc6f17f23282f81beef7a4
Bertrand Martin <97910213+bertrandmart1@users.noreply.github.com>
2022-10-03 17:59:17 +0200

    Create square.cpp

---
+++ c++/square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  
+  while (i < 10){
+    std::cout << i << "  " << i*i << "\n";
+    i++;
+
+  }
+  
+}
