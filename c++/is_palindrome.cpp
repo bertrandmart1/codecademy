55763d5b1f9a15b254c44a16f1a747b64e8b5ad7
Bertrand Martin <97910213+bertrandmart1@users.noreply.github.com>
2022-10-12 17:42:02 +0200

    Update is_palindrome.cpp

--- c++/is_palindrome.cpp
+++ c++/is_palindrome.cpp
@@ -2,8 +2,13 @@
 
 // Define is_palindrome() here:
 bool is_palindrome(std::string text){
+// Any string of 1 or 2 letters are palindromes
+ if (text.size() <= 2){
+  return true;
+}
+else {
  for ( int i = 0; i < text.size(); i++){
- if (text[i] == text[text.size()-(i+1)]){
+ if (text[i] == text[text.size()-(i+1)]){ // comparing each letter at the beginning and end, then beginning + 1 and end -1, etc
    return true;
  }
  else {
@@ -11,10 +16,11 @@ bool is_palindrome(std::string text){
  }
 }
 }
+}
 int main() {
   
   std::cout << is_palindrome("madam") << "\n";
-  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("annnn") << "\n";
   std::cout << is_palindrome("racecar") << "\n";
   
 }
