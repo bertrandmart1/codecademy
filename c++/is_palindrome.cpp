513f4b960f1d4ad535b19a347887dd4472d8a36b
Bertrand Martin <97910213+bertrandmart1@users.noreply.github.com>
2022-10-12 14:30:26 +0200

    Create is_palindrome.cpp

---
+++ c++/is_palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Define is_palindrome() here:
+bool is_palindrome(std::string text){
+ for ( int i = 0; i < text.size(); i++){
+ if (text[i] == text[text.size()-(i+1)]){
+   return true;
+ }
+ else {
+   return false;
+ }
+}
+}
+int main() {
+  
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("racecar") << "\n";
+  
+}
